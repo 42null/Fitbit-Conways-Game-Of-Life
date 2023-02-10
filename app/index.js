ddb3a084fc297ed691ff826100d8c0a0f40ee3d9
42null <averyparanoiduser@gmail.com>
2023-02-10 00:05:52 -0600

    Settings Companion Integration w/ adjustable colors & sizing

--- app/index.js
+++ app/index.js
@@ -1,22 +1,75 @@
+import * as messaging from "messaging";
 import * as document from "document";
 import { display } from "display";
 
+// SETTINGS COMPANION VARIABLES
 let background = document.getElementById("background");
+let border     = document.getElementById("border");
+
+let screenBackgroundColor = "#000000";
+let screenBorderColor     = "#000000";
+let liveCellColor         = "#FFFFFF";
+let deadCellColor         = "#000000";
+
+background.style.fill = screenBackgroundColor;
+border.style.fill     = screenBorderColor;
+
+
 
 const gameWidth = 300;
 const gameHeight = 300;
 // Max is 24 by 24 at the time due to updates per frame. Not sure if svg limmitation or Fitbit
-const gameTilesRow = 24;
-const gameTilesColoum = 24;
+let gameTilesRow = 24;
+let gameTilesColoum = 24;
 const GLIDER_TEST = false;
 const tickTimeMS = 1;
 const mathRandomMultiplyer = (GLIDER_TEST ? 0:2 );
-const gameTileWidth = gameWidth/gameTilesColoum;
-const gameTileHeight = gameHeight/gameTilesRow;
+let gameTileWidth = gameWidth/gameTilesColoum;
+let gameTileHeight = gameHeight/gameTilesRow;
 
 const cells = document.getElementsByClassName("cell");
-const array1 = Array.apply(null, Array(gameTilesRow*gameTilesColoum)).map(function () {return false;});
-const array2 = Array.apply(null, Array(gameTilesRow*gameTilesColoum)).map(function () {return false;});
+let array1 = Array.apply(null, Array(gameTilesRow*gameTilesColoum)).map(function () {return false;});
+let array2 = Array.apply(null, Array(gameTilesRow*gameTilesColoum)).map(function () {return false;});
+
+
+messaging.peerSocket.addEventListener("message", (evt) => {
+  console.log(JSON.stringify(evt.data));
+});
+
+
+messaging.peerSocket.addEventListener("message", (evt) => {
+  const key = evt.data.key;
+  const value = evt.data.value;
+
+  console.log("Key = "+evt.data.key);
+  console.log("Value = "+evt.data.value);
+  if(evt && evt.data){
+    if(evt.data.key === "KEY_COLOR__GRID_COLOR"){
+      screenBackgroundColor=value;
+      background.style.fill = screenBackgroundColor;
+    }else if(key === "KEY_COLOR__CELL_LIVE"){
+      liveCellColor=value;
+    }else if(key === "KEY_COLOR__CELL_DEAD"){
+      deadCellColor=value;
+    }else if(key === "KEY_NUMBER__COLOUMS"){
+      console.log("value = "+value)
+      gameTilesRow   =value;
+      gameTilesColoum=value;
+      gameTileWidth = gameWidth/gameTilesColoum;
+      gameTileHeight = gameHeight/gameTilesRow;
+      for(let i=0; i < cells.length; i+=1 ){
+        const cell = cells[i];
+        cell.y=-1;
+        cell.x=-1;
+        cell.width=0;
+        cell.height=0;
+      }
+      array1 = Array.apply(null, Array(gameTilesRow*gameTilesColoum)).map(function () {return false;});
+      array2 = Array.apply(null, Array(gameTilesRow*gameTilesColoum)).map(function () {return false;});
+      setBackground();
+    }
+  }
+});
 
 if(GLIDER_TEST){
   cells[3*gameTilesRow+2].style.fill="#FFFFFF";
@@ -35,40 +88,32 @@ if(GLIDER_TEST){
 console.log("array1.length = "+array1.length)
 console.log("array2.length = "+array2.length)
 console.log("cells.length  = "+cells.length);
-// for(var i=0; i<cells.length; i++){
-//   cells[i].style.fill="purple";
-// }
+
 
 const currentArray = array1;
+const currentArray2 = array2;
 
-for(var i=0; i<gameTilesRow; i++){
-  for(var j=0; j<gameTilesColoum; j++){
-    const cell = cells[i*gameTilesColoum+j];
-    const cellBoolean = currentArray[i*gameTilesColoum+j];
-    if(Math.floor(Math.random() * mathRandomMultiplyer)==0){
-      cell.style.fill="#FFFFFF";
-      // cellBoolean=true;
-      currentArray[i*gameTilesColoum+j]=true;
-    }else{
-      cell.style.fill="#FFFFFF";
-      // cell.style.fill="#000000";
-      // cellBoolean=false;
-      currentArray[i*gameTilesColoum+j]=false;
+function setBackground(){//TODO: Make take argments
+  for(var i=0; i<gameTilesRow; i++){
+    for(var j=0; j<gameTilesColoum; j++){
+      const cell = cells[i*gameTilesColoum+j];      
+      if(Math.floor(Math.random() * mathRandomMultiplyer)==0){
+        cell.style.fill=liveCellColor;
+        currentArray[i*gameTilesColoum+j]=true;
+        currentArray2[i*gameTilesColoum+j]=true;
+      }else{
+        cell.style.fill=deadCellColor;
+        currentArray[i*gameTilesColoum+j]=false;
+        currentArray2[i*gameTilesColoum+j]=false;
+      }
+      cell.y=gameTileHeight*i;
+      cell.x=gameTileWidth*j;
+      cell.width=gameTileWidth*.9;
+      cell.height=gameTileHeight*.9;
     }
-    if(i==0 && j==0){
-      cell.style.fill="#FF0000";
-    }else if(i==0){
-      cell.style.fill="#00FF00";
-    }else if(j==0){
-      cell.style.fill="#0000FF";
-     }
-    cell.y=gameTileHeight*i;
-    cell.x=gameTileWidth*j;
-    cell.width=gameTileWidth*.9;
-    cell.height=gameTileHeight*.9;
   }
 }
-
+setBackground();
 
 let iteration = 0;
 
@@ -118,9 +163,9 @@ for(var i=0; i<gameTilesRow; i++){
     const cell = cells[i*gameTilesColoum+j];
 
       if(arrayNext[i*gameTilesColoum+j]){
-         cell.style.fill="#FFFFFF";
+         cell.style.fill=liveCellColor;
       }else{
-         cell.style.fill="#000000";
+         cell.style.fill=deadCellColor;
       }
     }
    
--- companion/index.js
+++ companion/index.js
@@ -1,5 +1,33 @@
-/*
- * Entry point for the companion app
- */
+import { settingsStorage } from "settings";
+import * as messaging from "messaging";
+import { me as companion } from "companion";
 
-console.log("Companion code started");
+let KEY_COLOR = "myColor";
+
+// Settings have been changed
+settingsStorage.addEventListener("change", (evt) => {
+  sendValue(evt.key, evt.newValue);
+});
+
+// Settings were changed while the companion was not running
+if (companion.launchReasons.settingsChanged) {
+  // Send the value of the setting
+  sendValue(KEY_COLOR, settingsStorage.getItem(KEY_COLOR));
+}
+
+function sendValue(key, val) {
+  if (val) {
+    sendSettingData({
+      key: key,
+      value: JSON.parse(val)
+    });
+  }
+}
+function sendSettingData(data) {
+  // If we have a MessageSocket, send the data to the device
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  } else {
+    console.log("No peerSocket connection");
+  }
+}
